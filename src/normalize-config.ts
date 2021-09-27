2353f5a106bbd8f31468eb53ac35a880035c86f6
mukaiu <mukaiu@live.cn>
2021-09-27 14:32:04 +0800

    https://github.com/chatopera/osschat/issues/2 简化环境变量

--- src/normalize-config.ts
+++ src/normalize-config.ts
@@ -7,11 +7,11 @@ import {
 
 function normalizeConfig (config: WechatyChatoperaConfig): ChatoperaOptions {
 
-  const WECHATY_PLUGIN_CHATOPERA_CLIENTID = 'WECHATY_PLUGIN_CHATOPERA_CLIENTID'
-  const WECHATY_PLUGIN_CHATOPERA_SECRET   = 'WECHATY_PLUGIN_CHATOPERA_SECRET'
+  const CHATOPERA_CLIENTID = 'CHATOPERA_CLIENTID'
+  const CHATOPERA_SECRET   = 'CHATOPERA_SECRET'
 
-  const WECHATY_PLUGIN_CHATOPERA_BESTSCORE_THRESHOLD   = 'WECHATY_PLUGIN_CHATOPERA_BESTSCORE_THRESHOLD'
-  const WECHATY_PLUGIN_CHATOPERA_SUGGSCORE_THRESHOLD   = 'WECHATY_PLUGIN_CHATOPERA_SUGGSCORE_THRESHOLD'
+  const CHATOPERA_BESTSCORE_THRESHOLD   = 'CHATOPERA_BEST_REPLY_THRES'
+  const CHATOPERA_SUGGSCORE_THRESHOLD   = 'CHATOPERA_SUGG_REPLY_THRES'
 
   const language = config.language
 
@@ -20,11 +20,11 @@ function normalizeConfig (config: WechatyChatoperaConfig): ChatoperaOptions {
   let bestScoreThreshold = config.bestScoreThreshold
   let suggScoreThreshold = config.suggScoreThreshold
 
-  if (!clientId)     { clientId     = process.env[WECHATY_PLUGIN_CHATOPERA_CLIENTID]       }
-  if (!secret) { secret = process.env[WECHATY_PLUGIN_CHATOPERA_SECRET]  }
+  if (!clientId)     { clientId     = process.env[CHATOPERA_CLIENTID]       }
+  if (!secret) { secret = process.env[CHATOPERA_SECRET]  }
 
   if (!bestScoreThreshold) {
-    const threshold = process.env[WECHATY_PLUGIN_CHATOPERA_BESTSCORE_THRESHOLD]
+    const threshold = process.env[CHATOPERA_BESTSCORE_THRESHOLD]
     if (threshold) {
       bestScoreThreshold = parseFloat(threshold)
     } else {
@@ -33,7 +33,7 @@ function normalizeConfig (config: WechatyChatoperaConfig): ChatoperaOptions {
   }
 
   if (!suggScoreThreshold) {
-    const threshold = process.env[WECHATY_PLUGIN_CHATOPERA_SUGGSCORE_THRESHOLD]
+    const threshold = process.env[CHATOPERA_SUGGSCORE_THRESHOLD]
     if (threshold) {
       suggScoreThreshold = parseFloat(threshold)
     } else {
@@ -44,7 +44,7 @@ function normalizeConfig (config: WechatyChatoperaConfig): ChatoperaOptions {
   if (!clientId) {
     throw new Error(`
       Wechaty Chatopera Plugin requires ClientId for authorization.
-      Please set ${WECHATY_PLUGIN_CHATOPERA_CLIENTID} environment variable,
+      Please set ${CHATOPERA_CLIENTID} environment variable,
       or set 'clientId' in plugin config.
     `)
   }
@@ -52,7 +52,7 @@ function normalizeConfig (config: WechatyChatoperaConfig): ChatoperaOptions {
   if (!secret) {
     throw new Error(`
       Wechaty Chatopera Plugin requires Chatopera Secret.
-      Please set ${WECHATY_PLUGIN_CHATOPERA_SECRET} environment variable,
+      Please set ${CHATOPERA_SECRET} environment variable,
       or set 'secret' in plugin config.
     `)
   }
